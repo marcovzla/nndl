2041095c51718315691c3ade96c1bc1eeb269d5d
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2015-10-16 20:40:32 -0700

    renamed data dir

--- src/main/scala/nndl/MainApp.scala
+++ src/main/scala/nndl/MainApp.scala
@@ -2,10 +2,10 @@ package nndl
 
 object MainApp extends App {
 
-  val trainImagesPath = "data/train-images-idx3-ubyte.gz"
-  val trainLabelsPath = "data/train-labels-idx1-ubyte.gz"
-  val testImagesPath = "data/t10k-images-idx3-ubyte.gz"
-  val testLabelsPath = "data/t10k-labels-idx1-ubyte.gz"
+  val trainImagesPath = "mnist/train-images-idx3-ubyte.gz"
+  val trainLabelsPath = "mnist/train-labels-idx1-ubyte.gz"
+  val testImagesPath = "mnist/t10k-images-idx3-ubyte.gz"
+  val testLabelsPath = "mnist/t10k-labels-idx1-ubyte.gz"
 
   val trainDataset = DataReader.readDataset(trainImagesPath, trainLabelsPath)
   val testDataset = DataReader.readDataset(testImagesPath, testLabelsPath)
